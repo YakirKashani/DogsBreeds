7893a08513af261dc7bfc7cfb2dbad3004f74c84
yakir <kashani.yakir@gmail.com>
2025-01-27 13:02:12 +0200

    fixed version

--- DogBreedsLib/build.gradle.kts
+++ DogBreedsLib/build.gradle.kts
@@ -35,7 +35,7 @@ afterEvaluate() {
             create<MavenPublication>("release") {
                 groupId = "com.github.YakirKashani"
                 artifactId = "DogData"
-                version = "1.0.0"
+                version = "1.0.1"
                 artifact(tasks.getByName("bundleReleaseAar"))
                 pom {
                     withXml {
